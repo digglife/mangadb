0bb0181f48f621e5bc3e99be1145f5bc68c490ba
hajimuz <digglife@gmail.com>
2018-10-21 08:12:08 +0800

    添加漫画页面的解析功能

--- mangadb.py
+++ mangadb.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8
 import re
+from urllib.parse import urljoin
 import requests
-from urlparse import urljoin
 from bs4 import BeautifulSoup
 
 
@@ -18,12 +18,12 @@ class Book():
 
 class ManhuaDB():
 
-    def __init__(self, home):
-        self.home = home
+    HOME = 'https://www.manhuadb.com'
 
     def search(self, name, exact=False):
-        r = requests.get('https://www.manhuadb.com/search?q={}'.format(name)).text
-        html = BeautifulSoup(r)
+        search_url = urljoin(self.HOME, 'search?q={}'.format(name))
+        r = requests.get(search_url).text
+        html = BeautifulSoup(r, 'lxml')
         search_result_block = html.select_one('div.comic-main-section')
         result_title = search_result_block.find('div', class_='text-muted').text
         book_count = self.extract_count_from_text(result_title)
@@ -36,12 +36,12 @@ class ManhuaDB():
         for r in results:
             a = r.h2.a
             r_name = a.text
-            r_url = urljoin(self.home, a['href'])
+            r_url = urljoin(self.HOME, a['href'])
 
             if exact:
                 if r_name != name:
                     continue
-            book = parse_book(r_url)
+            book = self.parse_book(r_url)
             books.append(book)
 
         return books
@@ -51,6 +51,22 @@ class ManhuaDB():
 
     def parse_book(self, url):
         r = requests.get(url).text
+        html = BeautifulSoup(r, 'lxml')
+
+        meta_table = html.find('table', class_='comic-meta-data-table')
+        meta = {}
+        for tr in meta_table.find_all('tr'):
+            key = tr.find('th').text.strip()
+            td = tr.find('td')
+            a = td.find_all('a')
+            if a:
+                values = [i.text.strip() for i in a]
+                value = values[0] if len(values) == 1 else values
+            else:
+                value = td.text.strip()
+            meta[key] = value
+
+        return Book(meta)
 
     @staticmethod
     def extract_count_from_text(text):
