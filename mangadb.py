2cff766a184dc6b0278a393883711a5efce9788b
hajimuz <digglife@gmail.com>
2018-11-01 16:01:42 +0800

    过滤漫画属性

--- mangadb.py
+++ mangadb.py
@@ -7,21 +7,45 @@ from bs4 import BeautifulSoup
 
 
 class Book():
+
+    keys = [
+        'title',
+        'original-title',
+        'episodes',
+        'status',
+        'category',
+        'tags',
+        'age',
+        'description',
+    ]
+
     def __init__(self, meta):
         self.meta = meta
-        self.init()
+        self.init(meta)
 
+    def init(self, meta):
+        for k in self.keys:
+            setattr(self, k, meta.get(k, None))
 
-    def init(self):
-        for k, v in self.meta.items():
-            setattr(self, k, v)
+    def __repr__(self):
+        return "<Book(title:{})>".format(self.title)
 
 class ManhuaDB():
 
-    HOME = 'https://www.manhuadb.com'
+    def __init__(self):
+        self.home = 'https://www.manhuadb.com'
+        self.meta_map = {
+            '漫画名称': 'title',
+            '漫画原名': 'original-title',
+            '单行本数': 'episodes',
+            '连载状态': 'status',
+            '漫画分类': 'category',
+            '漫画标签': 'tags',
+            '面向读者': 'age'
+        }
 
     def search(self, name, exact=False):
-        search_url = urljoin(self.HOME, 'search?q={}'.format(name))
+        search_url = urljoin(self.home, 'search?q={}'.format(name))
         r = requests.get(search_url).text
         html = BeautifulSoup(r, 'lxml')
         search_result_block = html.select_one('div.comic-main-section')
@@ -36,7 +60,7 @@ class ManhuaDB():
         for r in results:
             a = r.h2.a
             r_name = a.text
-            r_url = urljoin(self.HOME, a['href'])
+            r_url = urljoin(self.home, a['href'])
 
             if exact:
                 if r_name != name:
@@ -47,7 +71,10 @@ class ManhuaDB():
         return books
 
     def search_one(self, name):
-        return self.search(name, exact=True)
+        books = self.search(name, exact=True)
+        if books:
+            return books[0]
+        return None
 
     def parse_book(self, url):
         r = requests.get(url).text
@@ -64,7 +91,11 @@ class ManhuaDB():
                 value = values[0] if len(values) == 1 else values
             else:
                 value = td.text.strip()
-            meta[key] = value
+            if key in self.meta_map:
+                meta[self.meta_map[key]] = value
+
+        div_detail_content = html.select_one('div.comic_detail_content > div')
+        meta['description'] = div_detail_content.decode_contents().strip()
 
         return Book(meta)
 
@@ -72,5 +103,3 @@ class ManhuaDB():
     def extract_count_from_text(text):
         m = re.search(r'\d+', text)
         return int(m.group())
-
-
